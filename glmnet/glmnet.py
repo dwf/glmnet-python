6c484694ff0d27779756b2c53417985ac0a95c9e
David Warde-Farley <dwf@cs.toronto.edu>
2010-05-03 19:50:43 -0400

    Some classes for wrapping results and predicting test data. I've given up on wrapping modval(), it is doing strange things and crashing. Also a plot_paths function for plotting the paths of the coefficients.

--- glmnet/glmnet.py
+++ glmnet/glmnet.py
@@ -74,4 +74,126 @@ def elastic_net(predictors, target, balance, memlimit=None,
         else:
             raise Exception('unknown error: %d' % jerr)
     
-    return lmu, a0, ca, ia, nin, rsq, alm, nlp, jerr
+    return GlmnetLinearResults(lmu, a0, ca, ia, nin, rsq, alm, nlp,
+                               predictors.shape[1], balance)
+
+
+class GlmnetLinearModel(object):
+    def __init__(self, a0, ca, ia, nin, rsq, alm, npred):
+        self._intercept = a0
+        self._coefficients = ca[:nin]
+        self._indices = ia[:nin] - 1
+        self._rsquared = rsq
+        self._lambda = alm
+        self._npred = npred
+
+    def __str__(self):
+        return ("%s with %d non-zero coefficients (%.2f%%)\n" + \
+                " * Intercept = %.7f, Lambda = %.7f\n" + \
+                " * Training r^2: %.4f") % \
+                (self.__class__.__name__, len(self._coefficients),
+                 len(self._coefficients) / float(self._npred) * 100,
+                 self._intercept, self._lambda, self._rsquared)
+
+    def predict(self, predictors):
+        predictors = np.atleast_2d(np.asarray(predictors))
+        return self._intercept + \
+                np.dot(predictors[:,self._indices], self._coefficients)
+    
+    @property
+    def coefficients(self):
+        coeffs = np.zeros(self._npred)
+        coeffs[self._indices] = self._coefficients
+        return coeffs
+
+
+
+class GlmnetLinearResults(object):
+    def __init__(self, lmu, a0, ca, ia, nin, rsq, alm, nlp, npred, parm):
+        self._lmu = lmu
+        self._a0 = a0
+        self._ca = ca
+        self._ia = ia
+        self._nin = nin
+        self._rsq = rsq
+        self._alm = alm
+        self._nlp = nlp
+        self._npred = npred
+        self._model_objects = {}
+        self._parm = parm
+
+    def __str__(self):
+        ninp = np.argmax(self._nin)
+        return ("%s object, elastic net parameter = %.3f\n" + \
+                " * %d values of lambda\n" + \
+            " * computed in %d passes over data\n" + \
+            " * largest model: %d predictors (%.1f%%), train r^2 = %.4f") % \
+            (self.__class__.__name__, self._parm, self._lmu, self._nlp, 
+             self._nin[ninp], self._nin[ninp] / float(self._npred) * 100, 
+             self._rsq[ninp]) 
+    
+    def __len__(self):
+        return self._lmu
+
+    def __getitem__(self, item):
+        item = (item + self._lmu) if item < 0 else item
+        if item >= self._lmu or item < 0:
+            raise IndexError("model index out of bounds")
+
+        if item not in self._model_objects:
+            model =  GlmnetLinearModel(
+                        self._a0[item],
+                        self._ca[:,item],
+                        self._ia,
+                        self._nin[item],
+                        self._rsq[item],
+                        self._alm[item],
+                        self._npred
+                    )
+            self._model_objects[item] = model
+        
+        else:
+            model = self._model_objects[item]
+        
+        return model
+    
+    @property
+    def nummodels(self):
+        return self._lmu
+
+    @property
+    def coefficients(self):
+        return self._ca[:np.max(self._nin), :self._lmu]
+
+    @property
+    def indices(self):
+        return self._ia
+
+def plot_paths(results, which_to_label=None):
+    import matplotlib
+    import matplotlib.pyplot as plt
+    plt.clf() 
+    interactive_state = plt.isinteractive()
+    for index, path in enumerate(results.coefficients):
+        if which_to_label and results.indices[index] in which_to_label:
+            if which_to_label[results.indices[index]] is None:
+                label = "$x_{%d}$" % results.indices[index]
+            else:
+                label = which_to_label[results.indices[index]]
+        else:
+            label = None
+        
+            
+        if which_to_label and label is None:
+            plt.plot(path, ':')
+        else:
+            plt.plot(path, label=label)
+    plt.xlim(0, results.nummodels - 1)
+    if which_to_label is not None:
+        plt.legend(loc='upper left')
+    plt.title('Regularization paths')
+    plt.xlabel('Model index (different values of $\\lambda$)')
+    plt.ylabel('Value of regression coefficient $\hat{\\beta}_i$')
+    
+    plt.show()
+    plt.interactive(interactive_state)
